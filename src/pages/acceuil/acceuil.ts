b8c34102cd381050e9460ee0e87075bab1402d3f
bennismehdi.officiel@gmail.com <mehdi.bennis01@etu.umontpellier.fr>
2018-04-06 18:16:40 +0200

    adding menu

--- src/pages/acceuil/acceuil.ts
+++ src/pages/acceuil/acceuil.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Article } from '../../classes/Article';
-
 /**
  * Generated class for the AcceuilPage page.
  *
