4a55ed81eae9041ba2faa4998a9679a49686344a
bennismehdi.officiel@gmail.com <mehdi.bennis01@etu.umontpellier.fr>
2018-04-24 12:26:13 +0200

    suppression d'import inutilisés 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class MyApp {
   rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar,
-              splashScreen: SplashScreen ,
+              splashScreen: SplashScreen
               ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -26,9 +26,9 @@ export class MyApp {
     });
   }
 
-     showProfil(){
+    showProfil(){
       this.nav.push(ProfilPage);
-  }
+    }
 }
 
   
